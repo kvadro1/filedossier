e5d47c1f8f9b173d967a0d49d228ac77b509cbc6
slavb18 <slavb18@gmail.com>
2019-06-10 09:06:01 +0400

    frontend stub

---
+++ filedossier-js/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import FileDossier from './FileDossier';
+import './App.css';
+
+class App extends Component {
+
+
+  render () {
+    return (
+      <div className="app">
+          <FileDossier dossierKey="123" dossierPackage="testmodel" dossierCode="TEST"/>
+      </div>
+    );
+  }
+}
+
+export default App;
