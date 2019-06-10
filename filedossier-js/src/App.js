3fcc4b45ee2de9c91576210360767cf5f28ab3d0
slavb18 <slavb18@gmail.com>
2019-06-10 15:01:18 +0400

    routes

--- filedossier-js/src/App.js
+++ filedossier-js/src/App.js
@@ -3,15 +3,33 @@ import Dossier from './Dossier';
 import Example from './Example';
 //import DisplayRemoteData from './DisplayRemoteData';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+    return (<div className="app">
+        <Dossier dossierKey="123" dossierPackage="testmodel" dossierCode="TEST"/>
+        <Example/>
+    </div>
+            );
+}
+
+function Activity(props) {
+    console.log(props.match.params);
+    return (<div className="app">
+    </div>
+            );
+}
 
 class App extends Component {
 
     render() {
         return (
-                <div className="app">
-                    <Dossier dossierKey="123" dossierPackage="testmodel" dossierCode="TEST"/>
-                    <Example/>
-                </div>
+                <Router>
+                    <div>
+                        <Route path="/" exact component={Index} />
+                        <Route path="/processes/:processId/activities/:activityId" component={Activity} />
+                    </div>
+                </Router>
                 );
     }
 }
