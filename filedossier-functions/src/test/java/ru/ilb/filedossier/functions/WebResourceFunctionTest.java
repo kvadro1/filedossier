fd33b6356f5e45523ee8b1f1c87940296bea4674
SPoket <lol8funny@gmail.com>
2019-06-26 11:35:31 +0000

    Change PdfGenRepresentation args
    git-svn-id: https://svn.ilb.ru/repos/filedossier/trunk@166 1ba50b13-9083-4a16-9cf7-98eefebc21bd

--- filedossier-functions/src/test/java/ru/ilb/filedossier/functions/WebResourceFunctionTest.java
+++ filedossier-functions/src/test/java/ru/ilb/filedossier/functions/WebResourceFunctionTest.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
