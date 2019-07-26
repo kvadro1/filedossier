7795c77dfc01481df69a2a24548003b602a80d72
SPoket <lol8funny@gmail.com>
2019-07-26 08:08:08 +0000

    Create FileStore's getAllContents() method returns list of files sorted by name
    git-svn-id: https://svn.ilb.ru/repos/filedossier/trunk@229 1ba50b13-9083-4a16-9cf7-98eefebc21bd

--- filedossier-store/src/main/java/ru/ilb/filedossier/store/FileStore.java
+++ filedossier-store/src/main/java/ru/ilb/filedossier/store/FileStore.java
@@ -15,25 +15,21 @@
  */
 package ru.ilb.filedossier.store;
 
-import ru.ilb.filedossier.entities.Store;
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import ru.ilb.filedossier.entities.Store;
 
 /**
  * Store using files
- * 
+ *
  * @author slavb
  */
 class FileStore implements Store {
@@ -45,72 +41,72 @@ class FileStore implements Store {
     private final URI storeRoot;
 
     public FileStore(URI storeRoot, String storeKey) {
-	this.storeKey = storeKey;
-	this.storeRoot = URI.create(storeRoot.toString() + "/");
+        this.storeKey = storeKey;
+        this.storeRoot = URI.create(storeRoot.toString() + "/");
     }
 
     /**
      * Получить путь к файлам хранилища
-     * 
+     *
      * @return hierarchical path of store files
      */
     private Path getStorePath() {
-	return Paths.get(storeRoot.resolve(storeKey));
+        return Paths.get(storeRoot.resolve(storeKey));
     }
 
     /**
      * Создать каталоги
      */
     private void createStorePath() {
-	getStorePath().toFile().mkdirs();
+        getStorePath().toFile().mkdirs();
 
     }
 
     private Path getFilePath(String key) {
-	if (!FILENAME_PREDICATE.test(key)) {
-	    throw new InvalidFileNameException(key);
-	}
-	return getStorePath().resolve(key);
+        if (!FILENAME_PREDICATE.test(key)) {
+            throw new InvalidFileNameException(key);
+        }
+        return getStorePath().resolve(key);
     }
 
     @Override
     public void setContents(String key, byte[] contents) throws IOException {
-	createStorePath();
-	Files.write(getFilePath(key), contents);
+        createStorePath();
+        Files.write(getFilePath(key), contents);
     }
 
     @Override
     public byte[] getContents(String key) throws IOException {
-	return Files.readAllBytes(getFilePath(key));
+        return Files.readAllBytes(getFilePath(key));
     }
 
     @Override
     public boolean isExist(String key) {
-	return getFilePath(key).toFile().exists();
+        return getFilePath(key).toFile().exists();
     }
 
     @Override
     public String toString() {
-	return "FileStore{" + "storeKey=" + storeKey + ", storeRoot=" + storeRoot + '}';
+        return "FileStore{" + "storeKey=" + storeKey + ", storeRoot=" + storeRoot + '}';
     }
 
     @Override
     public FileStore getNestedFileStore(String key) {
-	return new FileStore(getStorePath().toUri(), key);
+        return new FileStore(getStorePath().toUri(), key);
     }
 
     @Override
     public List<byte[]> getAllContents() throws IOException {
-	try (Stream<Path> paths = Files.walk(getStorePath())) {
-	    List<byte[]> bytes = new ArrayList<>();
-	    paths.filter(Files::isRegularFile).forEach(path -> {
-		try {
-		    bytes.add(Files.readAllBytes(path));
-		} catch (IOException ex) {
-		    throw new RuntimeException(ex);
-		}
-	    });
-	    return bytes;
-	}
+        try (Stream<Path> paths = Files.walk(getStorePath()).sorted()) {
+            List<byte[]> bytes = new ArrayList<>();
+            paths.filter(Files::isRegularFile).forEach(path -> {
+                try {
+                    bytes.add(Files.readAllBytes(path));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+            return bytes;
+        }
     }
 }
