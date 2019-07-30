77a1a4020b073bcb84dc4eb846b3852e95e46f68
Vjacheslav V. Borisov <slavb18@gmail.com>
2019-07-30 10:11:41 +0000

    описание прецедентов
    git-svn-id: https://svn.ilb.ru/repos/filedossier/trunk@235 1ba50b13-9083-4a16-9cf7-98eefebc21bd

--- docs/filedossier.sh
+++ docs/filedossier.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 umbrello5 --export png  --directory filedossier filedossier.xmi
-xsltproc /usr/share/umbrello5/xmi2docbook.xsl filedossier.xmi  > filedossier/filedossier.docbook 
+xsltproc --nonet /usr/share/umbrello5/xmi2docbook.xsl filedossier.xmi  > filedossier/filedossier.docbook
 pandoc -s --from docbook --to html --output  filedossier/filedossier.html filedossier/filedossier.docbook
