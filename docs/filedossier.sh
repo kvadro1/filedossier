19cdcdd438098554812cd938f7a1e654b1c9823f
Vjacheslav V. Borisov <slavb18@gmail.com>
2019-08-07 01:28:57 +0000

    добавил toc
    git-svn-id: https://svn.ilb.ru/repos/filedossier/trunk@251 1ba50b13-9083-4a16-9cf7-98eefebc21bd

--- docs/filedossier.sh
+++ docs/filedossier.sh
@@ -3,4 +3,4 @@ set -e
 FILE=`basename -s .sh $0`
 umbrello5 --export png  --directory ${FILE} ${FILE}.xmi
 xsltproc --nonet /usr/share/umbrello5/xmi2docbook.xsl ${FILE}.xmi  > ${FILE}/${FILE}.docbook
-pandoc -s --from docbook --to html --output  ${FILE}/${FILE}.html ${FILE}/${FILE}.docbook
+pandoc -s --toc --from docbook --to html --output  ${FILE}/${FILE}.html ${FILE}/${FILE}.docbook
